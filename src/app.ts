b306f9a6036aaaaca9e597feab980d8b28d40ffa
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2024-05-02 12:52:24 -0300

    :sparkles: feat: add user store route

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,27 @@
-import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.post('/users', async (req, res) => {
+    const registerBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6),
+    })
 
-prisma.user.create({
-    data: {
-        name: 'avcds',
-        email: 'avcds@email.com'
-    }
+    const { name, email, password } = registerBodySchema.parse(req.body)
+
+    const userCreated = await prisma.user.create({
+        data: {
+            name,
+            email,
+            password_hash: password
+        }
+    })
+
+    return res.status(201).send({
+        data: userCreated,
+    })
 })
