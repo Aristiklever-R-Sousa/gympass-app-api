b062d77238ccb041ad00437714fab64ef34df9e5
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2025-06-20 21:09:54 -0300

    :sparkles: feat: add refresh token cookie-based

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -28,9 +28,24 @@ export async function authenticate(req: Request, res: Response) {
             }
         })
 
-        return res.status(200).send({
-            token
+        const refreshToken = await res.jwtSign({}, {
+            sign: {
+                sub: user.id,
+                expiresIn: '7d',
+            },
         })
+
+        return res
+            .setCookie('refreshToken', refreshToken, {
+                path: '/',
+                secure: true,
+                sameSite: true,
+                httpOnly: true,
+            })
+            .status(200)
+            .send({
+                token
+            })
 
     } catch (err) {
         if (err instanceof InvalidCredentialsError) {
